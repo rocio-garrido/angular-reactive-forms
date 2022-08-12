c57c1717974b5ee24888f3fbde7f260932713c24
Rocio Garrido <rocio.garrido@wefox.com>
2022-08-12 10:07:45 +0200

    Set built-in validation rules

--- demo-start-v12/src/app/customers/customer.component.ts
+++ demo-start-v12/src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Customer } from './customer';
 
 @Component({
@@ -15,9 +15,9 @@ export class CustomerComponent implements OnInit {
 
   ngOnInit(): void {
     this.customerForm = this.fb.group({
-      firstName: '',
-      lastName: '',
-      email: '',
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
       sendCatalog: true
     });
   }
