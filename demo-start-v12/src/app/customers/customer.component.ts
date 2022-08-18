196059668344768b2438536243ee78003577b778
Rocio Garrido <rocio.garrido@wefox.com>
2022-08-18 09:23:54 +0200

    Reacting: adjusting validation rules

--- demo-start-v12/src/app/customers/customer.component.ts
+++ demo-start-v12/src/app/customers/customer.component.ts
@@ -51,7 +51,7 @@ export class CustomerComponent implements OnInit {
     });
 
     this.customerForm.get('notification')?.valueChanges.subscribe(
-      value => console.log(value)
+      value => this.setNotification(value)
     );
   }
 
