018dbd3ca13afec58d07ad28cc03c3ad91d6e318
Rocio Garrido <rocio.garrido@wefox.com>
2022-09-05 11:57:54 +0200

    Duplicate the input elements

--- demo-start-v12/src/app/customers/customer.component.ts
+++ demo-start-v12/src/app/customers/customer.component.ts
@@ -74,6 +74,10 @@ export class CustomerComponent implements OnInit {
     );
   }
 
+  addAddress(): void {
+    this.addresses.push(this.buildAddress());
+  }
+
   buildAddress(): FormGroup {
     return this.fb.group({
       addressType: 'home',
